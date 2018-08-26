22ab2e7e5b0b1671abd05629d69d738abb1b1167
Charles Korn <me@charleskorn.com>
2018-08-26 13:31:44 +1000

    Fix issue where application doesn't exit on Linux.

--- src/main/java/com/charleskorn/okhttpissue/Application.java
+++ src/main/java/com/charleskorn/okhttpissue/Application.java
@@ -2,7 +2,10 @@ package com.charleskorn.okhttpissue;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Comparator;
+import java.util.List;
 import java.util.logging.LogManager;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String... args) throws Exception {
@@ -13,6 +16,23 @@ public class Application {
         clientAndServer.run(Scenario.ChunkedWithoutEndMarker);
         clientAndServer.run(Scenario.ChunkedWithEndMarker);
         clientAndServer.run(Scenario.Normal);
+
+        List<Thread> foregroundThreads = Thread.getAllStackTraces().keySet().stream()
+                .filter(t -> !t.isDaemon())
+                .sorted(Comparator.comparing(Thread::getId))
+                .collect(Collectors.toList());
+
+        if (foregroundThreads.size() > 1) {
+            System.out.println("There is more than one foreground thread still running:");
+
+            for (Thread thread : foregroundThreads) {
+                System.out.println(thread.getId() + ": " + thread.getName());
+            }
+
+            System.out.println();
+            System.out.println("These will be forcibly terminated.");
+            Runtime.getRuntime().exit(0);
+        }
     }
 
     private static void configureLogging() throws IOException {
